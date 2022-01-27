4d9e3be73c53d1c155311b4c0e3abfec207ed93f
Begum Gunel <begumgunel@gmail.com>
2022-01-27 10:49:08 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Begüm Günel on 26.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
